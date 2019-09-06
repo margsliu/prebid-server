51510e6abccf60c79c635cba87fb20ba0d56bb5d
Margaret Liu <margs.liu@gmail.com>
2019-09-06 13:40:11 -0500

    Update usersync_test

--- adapters/lockerdome/usersync_test.go
+++ adapters/lockerdome/usersync_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestLockerDomeSyncer(t *testing.T) {
-	temp := template.Must(template.New("sync-template").Parse("https://lockerdome.com/usync/prebidserver?gdpr={{.GDPR}}&gdpr_consent={{.GDPRConsent}}&redirect=https%3A%2F%2Flocal.lockerdome.com%3A8888%2Fsetuid%3Fbidder%3Dlockerdome%26gdpr%3D{{.GDPR}}%26gdpr_consent%3D{{.GDPRConsent}}%26uid%3D%7B%7Buid%7D%7D"))
+	temp := template.Must(template.New("sync-template").Parse("https://lockerdome.com/usync/prebidserver?gdpr={{.GDPR}}&gdpr_consent={{.GDPRConsent}}&redirect=https%3A%2F%2Flocalhost%2Fsetuid%3Fbidder%3Dlockerdome%26gdpr%3D{{.GDPR}}%26gdpr_consent%3D{{.GDPRConsent}}%26uid%3D%7B%7Buid%7D%7D"))
 	syncer := NewLockerDomeSyncer(temp)
 	syncInfo, err := syncer.GetUsersyncInfo("", "")
 	assert.NoError(t, err)
-	assert.Equal(t, "https://lockerdome.com/usync/prebidserver?gdpr=&gdpr_consent=&redirect=https%3A%2F%2Flocal.lockerdome.com%3A8888%2Fsetuid%3Fbidder%3Dlockerdome%26gdpr%3D%26gdpr_consent%3D%26uid%3D%7B%7Buid%7D%7D", syncInfo.URL)
+	assert.Equal(t, "https://lockerdome.com/usync/prebidserver?gdpr=&gdpr_consent=&redirect=https%3A%2F%2Flocalhost%2Fsetuid%3Fbidder%3Dlockerdome%26gdpr%3D%26gdpr_consent%3D%26uid%3D%7B%7Buid%7D%7D", syncInfo.URL)
 	assert.Equal(t, "redirect", syncInfo.Type)
 	assert.EqualValues(t, 0, syncer.GDPRVendorID())
 	assert.Equal(t, false, syncInfo.SupportCORS)
